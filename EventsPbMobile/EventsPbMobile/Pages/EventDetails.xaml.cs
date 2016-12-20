98d1fb04daa15ad9dc1e7121a58cf227b5137f89
Vaviorky <danieloss15@gmail.com>
2016-12-20 09:44:35 +0100

    Added map with initial set to PB

--- EventsPbMobile/EventsPbMobile/Pages/EventDetails.xaml.cs
+++ EventsPbMobile/EventsPbMobile/Pages/EventDetails.xaml.cs
@@ -1,27 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EventsPbMobile.Models;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace EventsPbMobile.Pages
 {
     public partial class EventDetails : ContentPage
     {
-        private Event _event;
+        private readonly Event _event;
         public EventDetails(Event e)
         {
             this._event = e;
             InitializeComponent();
             Title = e.Title;
             Counter();
+            
         }
 
         private async void Counter()
         {
-            
+            var rt = _event.Date.Subtract(DateTime.Now);
+            while (rt.TotalSeconds > 0)
+            {
+                rt = _event.Date.Subtract(DateTime.Now);
+                DaysLabel.Text = rt.Days.ToString();
+                HoursLabel.Text = rt.Hours.ToString();
+                MinutesLabel.Text = rt.Minutes.ToString();
+                SecondsLabel.Text = rt.Seconds.ToString();
+                await Task.Delay(250);
+            }
+        }
+
+        private void MapButton_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new EventMap());
         }
     }
 }
---
+++ EventsPbMobile/EventsPbMobile/Pages/EventMap.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace EventsPbMobile.Pages
+{
+    public partial class EventMap : ContentPage
+    {
+        public EventMap()
+        {
+            InitializeComponent();
+            Title = "Mapa wydarzenia";
+            MyMap.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Position(53.118293, 23.149717), Distance.FromMeters(300)));
+        }
+    }
+}
